15b31b0a1697760b792d62833cdf29f87ef203ed
GarrettGenz <genzg@onid.orst.edu>
2017-06-23 03:28:35 -0700

    Bind to float 32

--- main.py
+++ main.py
@@ -9,4 +9,11 @@ train = pd.read_csv('data/train_2016_v2.csv')
 props = pd.read_csv('data/properties_2016.csv')
 sample = pd.read_csv('data/sample_submission.csv')
 
-print sample
+print ('Binding to float32...')
+
+for c, dtype in zip(props.columns, props.dtypes):
+    if dtype == np.float32:
+        props[c] = props[c].astype(np.float32)
+
+print ('Create training set...')
+
