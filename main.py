b005b3ad28242250067794d08288b352f14490d4
GarrettGenz <genzg@onid.orst.edu>
2017-06-25 08:17:40 -0700

    Build training data

--- main.py
+++ main.py
@@ -19,20 +19,36 @@ sample = pd.read_csv('data/sample_submission.csv')
 
 print ('Binding to float32...')
 
-drop_cols = ['parcelid']
+drop_cols = ['parcelid', 'logerror', 'transactiondate']
 one_hot_encode_cols = ['airconditioningtypeid', 'architecturalstyletypeid', 'buildingclasstypeid',
                        'heatingorsystemtypeid', 'storytypeid']
 
 for c, dtype in zip(props.columns, props.dtypes):
-    print c, dtype
-    s = props[c]
-    print s.head()
-    print s.describe()
+#    print c, dtype
+#    s = props[c]
+#    print s.head()
+#    print s.describe()
 
     if dtype == np.float32:
         props[c] = props[c].astype(np.float32)
 
 print ('Create training set...')
 
+df_train = train.merge(props, how='left', on='parcelid')
 
+x_train = df_train.drop(drop_cols, axis=1)
+y_train = df_train['logerror'].values
+print (x_train.shape, y_train.shape)
 
+train_cols = x_train.columns
+
+for c in x_train.dtypes[x_train.dtypes == object].index.values:
+    x_train[c] = (x_train[c] == True)
+
+del df_train; gc.collect()
+
+split = 80000
+
+x_train, y_train, x_valid, y_valid = x_train[:split], y_train[:split], x_train[split:], y_train[split:]
+
+print ('Building DMatrix...')
